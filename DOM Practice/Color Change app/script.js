5a4f864972f577f58656a61f90b4e9eee49c70d7
Easin Arafat <arafateassin1@gmail.com>
2026-06-03 18:14:16 +0600

    Completed this Color Change app

---
+++ DOM Practice/Color Change app/script.js
@@ -0,0 +1,36 @@
+const main = document.querySelector(".main")
+
+let first = document.querySelector(".first")
+first.addEventListener("click",  () => {
+    main.style.backgroundColor = "red";
+})
+
+let second = document.querySelector(".second")
+second.addEventListener("click",  () => {
+    main.style.backgroundColor = "blue";
+})
+
+let third = document.querySelector(".third")
+third.addEventListener("click",  () => {
+    main.style.backgroundColor = "green";
+})
+
+let fourth = document.querySelector(".fourth")
+fourth.addEventListener("click",  () => {
+    main.style.backgroundColor = "sandybrown";
+})
+
+let fifth = document.querySelector(".fifth")
+fifth.addEventListener("click",  () => {
+    main.style.backgroundColor = "teal";
+})
+
+let sixth = document.querySelector(".sixth")
+sixth.addEventListener("click",  () => {
+    main.style.backgroundColor = "blueviolet";
+})
+
+let seventh = document.querySelector(".seventh")
+seventh.addEventListener("click",  () => {
+    main.style.backgroundColor = "aqua";
+}) 
